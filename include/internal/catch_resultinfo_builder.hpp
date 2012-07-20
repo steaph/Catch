9149b9312449a812d7b15914ca839547a1fe2bc0
Martin Moene <martin.moene@gmail.com>
2012-07-20 23:09:38 +0200

    fixed: Friend access for class templates, issue #6

--- include/internal/catch_resultinfo_builder.hpp
+++ include/internal/catch_resultinfo_builder.hpp
@@ -1,5 +1,5 @@
 /*
- *  Created by Phil on 8/8/2012.
+ *  Created by Phil on 8/5/2012.
  *  Copyright 2012 Two Blue Cubes Ltd. All rights reserved.
  *
  *  Distributed under the Boost Software License, Version 1.0. (See accompanying
@@ -8,57 +8,104 @@
 #ifndef TWOBLUECUBES_CATCH_RESULTINFO_BUILDER_HPP_INCLUDED
 #define TWOBLUECUBES_CATCH_RESULTINFO_BUILDER_HPP_INCLUDED
 
-#include "catch_resultinfo_builder.h"
+#include "catch_tostring.hpp"
+#include "catch_resultinfo.hpp"
+#include "catch_result_type.h"
+#include "catch_evaluate.hpp"
+#include "catch_common.h"
 
 namespace Catch {
 
-    ResultInfoBuilder::ResultInfoBuilder() {}
+struct STATIC_ASSERT_Expression_Too_Complex_Please_Rewrite_As_Binary_Comparison;
+    
+class ResultInfoBuilder : public ResultInfo {
 
-    ResultInfoBuilder::ResultInfoBuilder(   const char* expr,
-                                            bool isNot,
-                                            const SourceLineInfo& lineInfo,
-                                            const char* macroName,
-                                            const char* message )
+public:
+    
+    ResultInfoBuilder() {}
+    
+    ResultInfoBuilder(  const char* expr, 
+                        bool isNot, 
+                        const SourceLineInfo& lineInfo,
+                        const char* macroName,
+                        const char* message = "" )
     : ResultInfo( expr, ResultWas::Unknown, isNot, lineInfo, macroName, message )
     {}
 
-    void ResultInfoBuilder::setResultType( ResultWas::OfType result ) {
+    void setResultType( ResultWas::OfType result ) {
         // Flip bool results if isNot is set
         if( m_isNot && result == ResultWas::Ok )
             m_result = ResultWas::ExpressionFailed;
         else if( m_isNot && result == ResultWas::ExpressionFailed )
             m_result = ResultWas::Ok;
         else
-            m_result = result;
+            m_result = result;        
     }
 
-    void ResultInfoBuilder::setMessage( const std::string& message ) {
+    void setMessage( const std::string& message ) {
         m_message = message;
     }
-
-    void ResultInfoBuilder::setLineInfo( const SourceLineInfo& lineInfo ) {
+    
+    void setLineInfo( const SourceLineInfo& lineInfo ) {
         m_lineInfo = lineInfo;
     }
-
-    void ResultInfoBuilder::setLhs( const std::string& lhs ) {
+    
+    void setLhs( const std::string& lhs ) {
         m_lhs = lhs;
     }
-
-    void ResultInfoBuilder::setRhs( const std::string& rhs ) {
+    
+    void setRhs( const std::string& rhs ) {
         m_rhs = rhs;
     }
 
-    void ResultInfoBuilder::setOp( const std::string& op ) {
+    void setOp( const std::string& op ) {
         m_op = op;
     }
+    
+    template<typename RhsT>
+    STATIC_ASSERT_Expression_Too_Complex_Please_Rewrite_As_Binary_Comparison& operator ||
+    (
+        const RhsT&
+    );
 
-    ResultInfoBuilder& ResultInfoBuilder::captureBoolExpression( bool result ) {
+    template<typename RhsT>
+    STATIC_ASSERT_Expression_Too_Complex_Please_Rewrite_As_Binary_Comparison& operator &&
+    (
+        const RhsT&
+    );
+    
+#ifdef INTERNAL_CATCH_COMPILER_IS_MSVC6
+public:
+#else
+private:
+    friend class ExpressionBuilder;
+    template<typename T> friend class Expression;
+
+    template<typename T> friend class PtrExpression;
+#endif
+
+    ResultInfoBuilder& captureBoolExpression( bool result ) {
         m_lhs = Catch::toString( result );
         m_op = m_isNot ? "!" : "";
         setResultType( result ? ResultWas::Ok : ResultWas::ExpressionFailed );
         return *this;
+    }    
+
+    template<Internal::Operator Op, typename T1, typename T2>    
+    ResultInfoBuilder& captureExpression( const T1& lhs, const T2& rhs ) {
+        setResultType( Internal::compare<Op>( lhs, rhs ) ? ResultWas::Ok : ResultWas::ExpressionFailed );
+        m_lhs = Catch::toString( lhs );
+        m_rhs = Catch::toString( rhs );
+        m_op = Internal::OperatorTraits<Op>::getName();
+        return *this;
     }
-    
+
+    template<Internal::Operator Op, typename T>
+    ResultInfoBuilder& captureExpression( const T* lhs, int rhs ) {
+        return captureExpression<Op>( lhs, reinterpret_cast<const T*>( rhs ) );
+    }    
+};
+
 } // end namespace Catch
 
 #endif // TWOBLUECUBES_CATCH_RESULTINFO_BUILDER_HPP_INCLUDED
