79ef412f442c94f411152c0838895133e5a5e843
Martin Moene <martin.moene@gmail.com>
2012-07-20 13:55:41 +0200

    changed: Added -m32 option to compile for 32bit on Windows 64-bit

--- projects/gcc/TestCatch/compile.bat
+++ projects/gcc/TestCatch/compile.bat
@@ -5,4 +5,4 @@
 :: Compile Catch selftests
 ::
 
-g++ -Wall -I../../../include -o TestCatch  ../../SelfTest/TrickyTests.cpp ../../SelfTest/ApproxTests.cpp ../../SelfTest/catch_self_test.cpp ../../SelfTest/catch_self_test.hpp ../../SelfTest/ClassTests.cpp ../../SelfTest/ConditionTests.cpp ../../SelfTest/ExceptionTests.cpp ../../SelfTest/GeneratorTests.cpp ../../SelfTest/MessageTests.cpp ../../SelfTest/MiscTests.cpp ../../SelfTest/TestMain.cpp
+g++ -m32 -Wall -I../../../include -o TestCatch  ../../SelfTest/TrickyTests.cpp ../../SelfTest/ApproxTests.cpp ../../SelfTest/catch_self_test.cpp ../../SelfTest/catch_self_test.hpp ../../SelfTest/ClassTests.cpp ../../SelfTest/ConditionTests.cpp ../../SelfTest/ExceptionTests.cpp ../../SelfTest/GeneratorTests.cpp ../../SelfTest/MessageTests.cpp ../../SelfTest/MiscTests.cpp ../../SelfTest/TestMain.cpp
