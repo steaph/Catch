65371232dc7b0852b4ff74bf0c945a22a90a525e
Martin Moene <martin.moene@gmail.com>
2012-07-20 15:33:11 +0200

    added: compile.bat for VC6 self test TestCatch.

---
+++ projects/VS6/TestCatch/compile.bat
@@ -0,0 +1,8 @@
+@echo off
+::
+:: Created by Martin on 20/07/2012.
+::
+:: Compile Catch selftests
+::
+
+cl -nologo -W3 -EHsc -GR -I../../../include -o TestCatch  ../../SelfTest/TrickyTests.cpp ../../SelfTest/ApproxTests.cpp ../../SelfTest/catch_self_test.cpp ../../SelfTest/catch_self_test.hpp ../../SelfTest/ClassTests.cpp ../../SelfTest/ConditionTests.cpp ../../SelfTest/ExceptionTests.cpp ../../SelfTest/GeneratorTests.cpp ../../SelfTest/MessageTests.cpp ../../SelfTest/MiscTests.cpp ../../SelfTest/TestMain.cpp  &&  TestCatch
