3d6c108db5b3127819c273ff99860b0ca177e8b2
Martin Moene <martin.moene@gmail.com>
2012-08-30 22:31:22 +0200

    fixed: Corrected compile.bat renamed: compile-main-debug.bat to compile-debug.bat removed: compile-main.bat

--- projects/VS6/TestCatch/compile-09-main.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-::
-:: Created by Martin on 25/07/2012.
-::
-:: Compile Catch selftests
-::
-
-cl -nologo -W3 -EHsc -GR -I../../../include -FeTestCatch.exe  ../../SelfTest/catch_self_test.cpp ../../SelfTest/TestMain.cpp ../../SelfTest/ApproxTests.cpp ../../SelfTest/BasicTests.cpp ../../SelfTest/ClassTests.cpp ../../SelfTest/ConditionTests.cpp ../../SelfTest/ExceptionTests.cpp ../../SelfTest/GeneratorTests.cpp ../../SelfTest/MessageTests.cpp ../../SelfTest/MiscTests.cpp ../../SelfTest/PointerTests.cpp ../../SelfTest/TrickyTests.cpp  &&  TestCatch
