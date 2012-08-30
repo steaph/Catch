56f11f0426efefb47a56e4f172ed9caddee21ddc
Martin Moene <martin.moene@gmail.com>
2012-08-30 15:22:17 +0200

    added: compile-09-main-debug.bat

---
+++ projects/VS6/TestCatch/compile-09-main-debug.bat
@@ -0,0 +1,8 @@
+@echo off
+::
+:: Created by Martin on 25/07/2012.
+::
+:: Compile Catch selftests
+::
+
+cl -nologo -W3 -EHsc -GR -GZ -Zd -MLd -I../../../include -FeTestCatch.exe  ../../SelfTest/catch_self_test.cpp ../../SelfTest/ApproxTests.cpp ../../SelfTest/BasicTests.cpp ../../SelfTest/ClassTests.cpp ../../SelfTest/ConditionTests.cpp ../../SelfTest/ExceptionTests.cpp ../../SelfTest/GeneratorTests.cpp ../../SelfTest/MessageTests.cpp ../../SelfTest/MiscTests.cpp ../../SelfTest/PointerTests.cpp ../../SelfTest/TestMain.cpp ../../SelfTest/TrickyTests.cpp  &&  TestCatch
