4aeb6a59ccde1a3eb24b136b217cbdbff3cb49f9
Martin Moene <martin.moene@gmail.com>
2012-09-15 23:53:51 +0200

    Change order of source files to match VC6 compilation

--- projects/gcc/TestCatch/compile.bat
+++ projects/gcc/TestCatch/compile.bat
@@ -5,4 +5,4 @@
 :: Compile Catch selftests
 ::
 
-g++ -m32 -Wall -I../../../include -o TestCatch  ../../SelfTest/TrickyTests.cpp ../../SelfTest/ApproxTests.cpp ../../SelfTest/catch_self_test.cpp ../../SelfTest/catch_self_test.hpp ../../SelfTest/ClassTests.cpp ../../SelfTest/ConditionTests.cpp ../../SelfTest/ExceptionTests.cpp ../../SelfTest/GeneratorTests.cpp ../../SelfTest/MessageTests.cpp ../../SelfTest/MiscTests.cpp ../../SelfTest/TestMain.cpp  && TestCatch
+g++ -m32 -Wall -I../../../include -o TestCatch  ../../SelfTest/catch_self_test.cpp ../../SelfTest/TestMain.cpp ../../SelfTest/ApproxTests.cpp ../../SelfTest/BasicTests.cpp ../../SelfTest/ClassTests.cpp ../../SelfTest/ConditionTests.cpp ../../SelfTest/ExceptionTests.cpp ../../SelfTest/GeneratorTests.cpp ../../SelfTest/MessageTests.cpp ../../SelfTest/MiscTests.cpp ../../SelfTest/PointerTests.cpp ../../SelfTest/TrickyTests.cpp  &&  TestCatch
