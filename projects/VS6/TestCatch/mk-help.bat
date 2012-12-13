87222e7014f9765b65347f6cf6340d5b4b6eb0ec
Martin Moene <martin.moene@gmail.com>
2012-12-13 13:22:14 +0100

    Fix command to collect help texts Remove old script collect-help.bat

--- projects/VS6/TestCatch/mk-help.bat
+++ projects/VS6/TestCatch/mk-help.bat
@@ -1,1 +1,8 @@
-TestCatch -t -g -l -r -o -s -b -n -a -n -w -? >TestCatch.help.txt
+@echo off
+::
+:: Created by Martin on 21/09/2012.
+::
+:: Collect all embedded help texts.
+::
+TestCatch -h >TestCatch.help.txt
+TestCatch -h -t -g -l -r -o -s -b -n -a -n -w -? >>TestCatch.help.txt
