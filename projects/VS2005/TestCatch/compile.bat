625a4f05e350f5e7a5e3cb7d4316edbf2b56ffc2
Martin Moene <martin.moene@gmail.com>
2013-04-22 21:52:46 +0200

    Add Visual Studio 2005 project

---
+++ projects/VS2005/TestCatch/compile.bat
@@ -0,0 +1,18 @@
+@echo off
+::
+:: Created by Martin on 20/07/2012.
+::
+:: Compile Catch selftests
+::
+
+::
+:: Note: in several stages of its development, Catch-VC6 gives a memory access
+:: violation just (code 0xC0000005) before the program ends (all output is there).
+:: When a selection of tests is specified, the problem does not show.
+:: The access violation is seen when linking to the default runtime library
+:: LIBC.LIB, option /ML and with LIBCMT.LIB, option /MT.
+:: Linking to MSVCRT.LIB, option /MD, made the (detection of the) access
+:: violation go away.
+::
+
+cl -nologo -W3 -EHsc -GR -MD -I../../../include -I../../../include/internal -FeTestCatch.exe  ../../SelfTest/catch_self_test.cpp ../../SelfTest/TestMain.cpp ../../SelfTest/ApproxTests.cpp ../../SelfTest/BasicTests.cpp ../../SelfTest/BDDTests.cpp ../../SelfTest/ClassTests.cpp ../../SelfTest/ConditionTests.cpp ../../SelfTest/ExceptionTests.cpp ../../SelfTest/GeneratorTests.cpp ../../SelfTest/MessageTests.cpp ../../SelfTest/MiscTests.cpp ../../SelfTest/PointerTests.cpp ../../SelfTest/TrickyTests.cpp  &&  TestCatch
