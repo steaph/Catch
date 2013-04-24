d7a1d61feac9aef43ba4315243e85ff68b050ef0
Martin Moene <martin.moene@gmail.com>
2013-04-24 10:51:14 +0200

    Call clang++ batch file to also run resulting program

--- projects/clang/TestCatch/compile.bat
+++ projects/clang/TestCatch/compile.bat
@@ -5,4 +5,4 @@
 :: Compile Catch selftests
 ::
 
-clang -m32 -Wall -I../../../include -o TestCatch  ../../SelfTest/catch_self_test.cpp ../../SelfTest/TestMain.cpp ../../SelfTest/ApproxTests.cpp ../../SelfTest/BasicTests.cpp ../../SelfTest/BDDTests.cpp ../../SelfTest/ClassTests.cpp ../../SelfTest/ConditionTests.cpp ../../SelfTest/ExceptionTests.cpp ../../SelfTest/GeneratorTests.cpp ../../SelfTest/MessageTests.cpp ../../SelfTest/MiscTests.cpp ../../SelfTest/PointerTests.cpp ../../SelfTest/TrickyTests.cpp ../../SelfTest/VariadicMacrosTests.cpp  &&  TestCatch
+call clang++ -m32 -Wall -I../../../include -o TestCatch  ../../SelfTest/catch_self_test.cpp ../../SelfTest/TestMain.cpp ../../SelfTest/ApproxTests.cpp ../../SelfTest/BasicTests.cpp ../../SelfTest/BDDTests.cpp ../../SelfTest/ClassTests.cpp ../../SelfTest/ConditionTests.cpp ../../SelfTest/ExceptionTests.cpp ../../SelfTest/GeneratorTests.cpp ../../SelfTest/MessageTests.cpp ../../SelfTest/MiscTests.cpp ../../SelfTest/PointerTests.cpp ../../SelfTest/TrickyTests.cpp ../../SelfTest/VariadicMacrosTests.cpp  &&  TestCatch
